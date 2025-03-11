5ea94cf85a3a96e824fe5d5c88b770197208dbd7
Rodrigo-Brezolin-Buquera <rodrigobbuquera@gmail.com>
2025-03-11 18:10:26 -0300

    init - setup backend

---
+++ backend/src/index.ts
@@ -0,0 +1,23 @@
+import express from "express"
+import "express-async-errors";
+import cors from "cors"
+import { AddressInfo } from "net"
+
+
+export const app = express()
+app.use(express.json())
+app.use(cors())
+
+const server = app.listen(3003, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Server is running in http://localhost:${address.port}`);
+  } else {
+    console.error("Failure upon starting server.");
+  }
+})
+
+
+app.get('/', (req, res) => {
+    res.send('Backend Express funcionando!');
+});
