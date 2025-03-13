1c4c6e2bc3e380a8a1ddf3a7b993ba86457493dd
Rodrigo-Brezolin-Buquera <rodrigobbuquera@gmail.com>
2025-03-13 07:51:37 -0300

    config - novas transações iniciais

--- backend/src/database/sync.ts
+++ backend/src/database/sync.ts
@@ -12,9 +12,11 @@ export async function syncDatabase() {
     ]);
 
     await Transaction.bulkCreate([
-      { cpf: "123.456.789-00", description: "Venda do produto X", transaction_date: new Date("2022-10-10"), point_value: 10000, value: 10000.00, status: "Aprovado" },
-      { cpf: "123.456.789-00", description: "Venda do produto Y", transaction_date: new Date("2022-10-10"), point_value: 10000, value: 10000.00, status: "Reprovado" },
-      { cpf: "123.456.789-00", description: "Venda do produto Z", transaction_date: new Date("2022-10-10"), point_value: 10000, value: 10000.00, status: "Em avaliação" }
+      { cpf: "123.456.789-00", description: "Venda de X", transaction_date: new Date("2022-10-10"), point_value: 10000, value: 10000.00, status: "Aprovado" },
+      { cpf: "123.456.789-00", description: "Venda da Y", transaction_date: new Date("2022-10-10"), point_value: 833, value: 833.00, status: "Reprovado" },
+      { cpf: "123.456.789-00", description: "Venda do Z", transaction_date: new Date("2022-10-10"), point_value: 10000, value: 10000.00, status: "Em avaliação" },
+      { cpf: "432.54.222-00", description: "Venda do AA", transaction_date: new Date("2022-10-10"), point_value: 500, value: 500.00, status: "Reprovado" },
+      { cpf: "34.456.33-00", description: "Venda do BB", transaction_date: new Date("2022-10-10"), point_value: 100, value: 100.00, status: "Em avaliação" }
     ]);
 
     console.log("Banco sincronizado e dados inseridos!");
